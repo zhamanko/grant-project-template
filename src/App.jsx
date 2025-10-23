b1ace84f0bd0464800bb93ad697df2b755eadecc
zhamanko <slavko.lenko28.06.2016@gmail.com>
2025-10-23 10:38:08 +0300

    feat: initialize project with Vite, React, and Tailwind CSS
    - Added package.json with dependencies for React, Tailwind CSS, and Vite.
    - Included Vite logo SVG in public directory.
    - Created main application component (App.jsx) with a simple greeting.
    - Added React logo SVG in assets directory.
    - Developed Header component with navigation links.
    - Set up main CSS file to import Tailwind CSS styles.
    - Configured entry point (main.jsx) for React application.
    - Established Vite configuration for React and Tailwind CSS integration.

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <main>
+        <h1>Привіт</h1>
+      </main>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+export default function Header() {
+  return (
+    <>
+      <header className="fixed top-0 w-full h-20 z-20 bg-gray-300">
+        <nav>
+            <ul>
+                <li>Головна</li>
+                <li>Новити</li>
+                <li>Події</li>
+                <li>Галерея</li>
+            </ul>
+        </nav>
+      </header>
+    </>
+  );
+}
